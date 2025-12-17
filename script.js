48c25b0602b7016c93f4c2bf4d0db1ab4056287c
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-12-17 10:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,37 +31,3 @@ function stopSounds() {
     audio.currentTime = 0; 
   });
 }
-//your JS code here. If required.
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
-
-const buttonsDiv = document.getElementById("buttons");
-
-sounds.forEach(sound => {
-  const btn = document.createElement("button");
-  btn.classList.add("btn");
-  btn.innerText = sound;
-
-  btn.addEventListener("click", () => {
-    stopSounds();
-    const audio = document.getElementById(sound);
-    audio.play();
-  });
-
-  buttonsDiv.appendChild(btn);
-
-  const audio = document.createElement("audio");
-  audio.id = sound;
-  audio.src = `./sounds/${sound}.mp3`;
-  document.body.appendChild(audio);
-});
-
-// Stop button functionality
-document.querySelector(".stop").addEventListener("click", stopSounds);
-
-function stopSounds() {
-  sounds.forEach(sound => {
-    const audio = document.getElementById(sound);
-    audio.pause();
-    audio.currentTime = 0; 
-  });
-}
